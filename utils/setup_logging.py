f7437f6b666306a02bd1cad58d57501d9532fa36
Charles Bokor <charlesBokor@outlook.com>
2025-07-02 14:55:07 +0100

    Added logging module in utils, including logging template now & config template in future

---
+++ utils/setup_logging.py
@@ -0,0 +1,37 @@
+# %% Import
+
+import os
+import shutil
+from datetime import datetime
+from torch.utils.tensorboard.writer import SummaryWriter
+
+# %% Methods
+
+
+def setup_logging_dir(config_path, root_dir="runs", exp_name=None):
+
+    if exp_name is None:
+        exp_name = "default_experiment"
+
+    # Create a timestamp for the experiment
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    log_dir = os.path.join(root_dir, exp_name, timestamp)
+
+    # Make directories if they do not exist
+    os.makedirs(log_dir, exist_ok=True)
+    os.makedirs(os.path.join(log_dir, "checkpoints"), exist_ok=True)
+    os.makedirs(os.path.join(log_dir, "best_model"), exist_ok=True)
+    os.makedirs(os.path.join(log_dir, "logs"), exist_ok=True)
+    # os.makedirs(os.path.join(log_dir, "samples"), exist_ok=True)
+
+    # Copy the config file to the log directory
+    shutil.copy(config_path, os.path.join(log_dir, "config.yaml"))
+
+    # Initialize TensorBoard writer
+    writer = SummaryWriter(log_dir=log_dir)
+
+    return log_dir, writer
+
+
+def config_template():
+    pass
